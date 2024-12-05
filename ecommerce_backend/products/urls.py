665d6d9489cab968cca3d06bd9e8a7b14b4c205d
vilhood <145817335+vilhood@users.noreply.github.com>
2024-12-05 15:15:34 -0500

    agregar la carpeta urls a products

--- ecommerce_backend/products/urls.py
+++ ecommerce_backend/products/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import SearchView
+
+urlpatterns = [
+    # Endpoint para buscar productos con paginación y filtros
+    path('products/', SearchView.as_view(), name='product-search'),
+]
 
